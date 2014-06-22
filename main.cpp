a3183cc40c683cbf31fb181107613e76d932af37
quartorz <tsuhaso@gmail.com>
2014-06-22 11:24:55 +0900

    spec3: 仕様3を実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "オムライス" << std::endl;
+	if(argc < 2)
+		return 0;
+
+	std::ifstream data(argv[1]);
+	std::string recipe;
+
+	std::getline(data, recipe);
+	std::cout << recipe << std::endl;
 }
