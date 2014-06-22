3a7bc29faae0e10bb37ec714ed561ac932fdcb31
quartorz <tsuhaso@gmail.com>
2014-06-22 12:00:57 +0900

    spec6: 仕様6を実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
 	if(argc < 2)
 		return 0;
 
-	std::ifstream data(argv[1]);
-	std::string recipe;
+	std::vector<std::string> recipes;
 
-	for(int id = 1; id <= 3; id++){
-		std::getline(data, recipe);
-		std::cout << id << ": " << recipe << std::endl;
+	// Input
+	{
+		std::ifstream data(argv[1]);
+		std::string recipe;
+
+		for(int i = 0; i < 3; i++){
+			std::getline(data, recipe);
+			recipes.push_back(recipe);
+		}
+	}
+
+	// Output
+	if(argc < 3){
+		for(int idx = 0; idx < 3; idx++){
+			int id = idx + 1;
+			std::cout << id << ": " << recipes[idx] << std::endl;
+		}
+	}else{
+		int id = std::atoi(argv[2]);
+		int idx = id - 1;
+		std::cout << id << ": " << recipes[idx] << std::endl;
 	}
 }
